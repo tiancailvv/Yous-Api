c3018faa8c59357e557f347a71109600b0f97b19
zhouwei <zhouwei163mail@163.com>
2016-11-07 10:05:27 +0800

    新建项目

---
+++ Yous-API/Controllers/APIController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using Yous;
+using Yous_Api.Models;
+
+namespace Yous_API.Controllers
+{
+    public class APIController : ApiController
+    {
+        [Route("api/GetServiceApiResult")]
+        [HttpPost]
+        public ObjResult GetServiceApiResult(dynamic obj)
+        {
+            IDictionary<string, object> route = new Dictionary<string, object>();
+            UrlHelper helper = new UrlHelper(Request);
+            route["controller"] = "Figure";
+            route["action"] = "GetAll";
+            string AbsoluteUrl = helper.Link("DefaultApi", route);
+            return  RedirectToRoute("api/10000001", obj);
+            return CreatedAtRoute("Post", new { }, obj);
+ 
+        }
+
+        [Route("api/10000001")]
+        [HttpPost]
+        public ObjResult GetUser(dynamic obj)
+        {
+            ObjResult result = new ObjResult();
+            result.success = true;
+            result.message = "ok";
+            MySqlDbHelperDB dbhelper = new MySqlDbHelperDB();
+            result.data = dbhelper.Fetch<base_area>("select * from base_area");
+            return result;
+        }
+
+    }
+}
---
+++ Yous-API/Models/RequestJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Yous_Api.Models
+{
+    public class RequestJson
+    {
+        public object parameters { get; set; }
+        public string foreEndType { get; set; }
+        public string code { get; set; }
+    }
+}
