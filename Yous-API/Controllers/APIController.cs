3ddf53de9d6f06ec8565a76dc119eb17c86b2781
zhouwei <zhouwei163mail@163.com>
2016-11-07 22:23:45 +0800

    修改api基础类

--- Yous-API/Controllers/APIController.cs
+++ Yous-API/Controllers/APIController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Routing;
 using Yous;
@@ -14,23 +16,47 @@ namespace Yous_API.Controllers
     {
         [Route("api/GetServiceApiResult")]
         [HttpPost]
-        public ObjResult GetServiceApiResult(dynamic obj)
+        public IHttpActionResult GetServiceApiResult(RequestJson obj)
         {
-            IDictionary<string, object> route = new Dictionary<string, object>();
-            UrlHelper helper = new UrlHelper(Request);
-            route["controller"] = "Figure";
-            route["action"] = "GetAll";
-            string AbsoluteUrl = helper.Link("DefaultApi", route);
-            return  RedirectToRoute("api/10000001", obj);
-            return CreatedAtRoute("Post", new { }, obj);
- 
+            string ret = String.Empty;
+
+            #region 发送Request请求
+            DateTime reqeustdt = DateTime.Now;
+            //Logs.kufaLog("前端调用类型：" + foreendtype + "\r\n前端请求地址：" + target + "\r\n开始发送请求：" + param, WebConfigurationManager.AppSettings["LogUrl"], logfoldername);
+            HttpWebRequest proxyRequest = HttpWebRequest.Create("http://"+Url.Request.Headers.Host+"/api/" + obj.code) as HttpWebRequest;
+            proxyRequest.Method = "POST";
+            proxyRequest.KeepAlive = false;
+            proxyRequest.ContentType = "application/json";
+            proxyRequest.Timeout = 200000;
+
+            byte[] aryBuf = Encoding.GetEncoding("utf-8").GetBytes(obj.ToString());
+            proxyRequest.ContentLength = aryBuf.Length;
+            using (Stream writer = proxyRequest.GetRequestStream())
+            {
+                writer.Write(aryBuf, 0, aryBuf.Length);
+                writer.Close();
+                writer.Dispose();
+            }
+        
+            #endregion
+
+            #region 返回Response
+            using (WebResponse response = proxyRequest.GetResponse())
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+                ret = reader.ReadToEnd();
+                reader.Close();
+                reader.Dispose();
+            }
+            #endregion
+            return new TextResult(ret, Request);
         }
 
         [Route("api/10000001")]
         [HttpPost]
-        public ObjResult GetUser(dynamic obj)
+        public ResponseJson GetUser(dynamic obj)
         {
-            ObjResult result = new ObjResult();
+            ResponseJson result = new ResponseJson();
             result.success = true;
             result.message = "ok";
             MySqlDbHelperDB dbhelper = new MySqlDbHelperDB();
